4c47ffb764799e4c4efd73fae7e788b381ae9ab6
Vasilevski95 <djordje.vasilevski95@gmail.com>
2023-05-04 15:21:31 +0200

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./state/theme.js";
+import LoginPage from "scenes/Login page/LoginPage.jsx";
+import HomePage from "scenes/Home page/HomePage.jsx";
+import ProfilePage from "scenes/Profile page/ProfilePage.jsx";
+
+function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/scenes/Widgets/AdvertWidget.jsx
@@ -0,0 +1,40 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWidget = () => {
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        alt="advert"
+        src="http://localhost:3001/assets/PORTFOLIO.png"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>Đorđe Vasilevski</Typography>
+        <Typography color={medium}>portfolio-vasilevski.netlify.app</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        Do you need a web developer who can work collaboratively with your team
+        and communicate effectively with stakeholders? As a skilled communicator
+        and team player, I can ensure that your project is delivered on time,
+        within budget, and to your exact specifications. Feel free to send an
+        email: djordje.vasilevski95@gmail.com
+      </Typography>
+    </WidgetWrapper>
+  );
+};
+
+export default AdvertWidget;
